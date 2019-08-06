783daac309e56ee0b6c1390aec1fcdf35c11ffde
zhangzhijie <uestczzj@163.com>
2019-08-06 16:05:36 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 
-/usr/bin/aria2c --conf-path=/root/91porn/aria2.conf -D \
+PROXY=${PROXY:-""}
+
+/usr/bin/aria2c --all-proxy=${PROXY} --conf-path=/root/91porn/aria2.conf -D \
 && darkhttpd /root/aria2-ng/ --port 80 --daemon \
 && darkhttpd /root/91porn/videos/ --port 8080
